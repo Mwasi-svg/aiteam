cd38d031d126f611686ded86dfbabe7c47c11a5b
Turi <iman.bulls@gmail.com>
2025-04-23 23:06:23 +0300

    analyst update

--- analysis_agent/main.py
+++ analysis_agent/main.py
@@ -64,4 +64,5 @@ def run():
     return jsonify({"result": result})
 
 if __name__ == "__main__":
-    app.run(port=5004)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0", port=port)
